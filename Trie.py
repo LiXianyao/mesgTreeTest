00bfe117db63733705c57315aad10c5e1a6c832a
Wayne Li <18976753003@163.com>
2017-07-03 00:08:31 +0800

    first commit

---
+++ Trie.py
@@ -0,0 +1,44 @@
+#-*-coding:utf-8-*-#
+
+class TrieNode:
+    def __init__(self,keyword):
+        self.keyword = keyword  #该节点对应的关键词
+        self.list = []          #能与该节点匹配成功的句子编号列表
+        self.children = {}     #该节点的子节点列表,"子节点的词"：子节点对象
+
+    def addChild(self,childNode):
+        #children中插入一个新点，dict的key为节点的关键词
+        self.children[childNode.keyword] = childNode
+
+    def getListLen(self):
+        return len(self.list)
+
+    def addList(self,no):
+        if no in self.list:
+            return
+        self.list.append(no)
+
+    def matchWord(self,Mesg,lenMesg,loc,no): #递归回溯法插入节点
+        #print "%s %s %d %d %d"%(self.keyword,Mesg,lenMesg,loc,no)
+        if loc == lenMesg: #递归到了句子结尾，结束
+            return
+
+        while loc<lenMesg: #对每一层，它都试图和字符串当前位置之后的每个词为开头的子句做一次匹配
+            if Mesg[loc] in self.children: #当前节点的后代中有可用匹配上的词
+                next = self.children.get(Mesg[loc]) #取出对应的字典树节点
+                next.addList(no)                    #添加匹配句子编号队列
+                next.matchWord(Mesg,lenMesg,loc+1,no)  #对这个对象递归处理
+            else:#当前节点的后代中没有以 Mesg[loc]开头的子句
+                newNode = TrieNode(Mesg[loc])
+                self.addChild(newNode)  #新点是当前点的一个新后代
+                newNode.addList(no)     #新句子当然包含了本身
+                newNode.matchWord(Mesg,lenMesg,loc+1,no) #对新点递归处理
+            #当前点匹配下一个位置
+            loc += 1
+
+    def Trans(self,roadRecord):
+        for childkey in self.children:
+            child = self.children[childkey]
+            child.Trans(roadRecord+child.keyword+",")
+
+        print ("%s %d %s")%(roadRecord,self.getListLen(),self.list)
---
+++ main.py
@@ -0,0 +1,24 @@
+#-*-coding:utf-8-*-#
+from Trie import TrieNode
+resultDict = {} #存储结果的字典结构
+
+def insertMsg(rootNode,Msg,no):
+    #root是字典树的根节点，Msg是某条短信（只有关键词部分的内容）,no是句子的序号（保存在树中用来标识结果模板都有哪些句子）
+    loc = 0
+    lenMsg = len(Msg)
+    now = rootNode
+    rootNode.matchWord(Msg,lenMsg,loc,no) #从根节点开始，以回溯法把句子Msg插入字典树,
+
+if __name__ == '__main__':
+    #这里将输入数据读入（已分词？未分词？）
+    keywords = []  # 假设这里存关键词
+    sentenses = [['我','是','中国人'],['中国人','是','我'],['我','爱','二次元'],['我','是','北邮人'],['我','爱','北邮'],['我','爱','熬夜','写','代码']]  # 这里存短信句子
+    #省略对句子内容的过滤部分，即先认为此时的sentenses已经是只含有关键词的句子集合
+
+    rootNode = TrieNode("")  #根节点
+    lenSen = len(sentenses)
+    for i in range(0,lenSen):
+        insertMsg(rootNode,sentenses[i],i) #将句子依次插入字典树
+
+    #遍历树看看情况
+    rootNode.Trans("")
