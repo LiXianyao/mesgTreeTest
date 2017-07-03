21168c2597e9f632b9ed32c1c643043e32e3585c
Wayne Li <18976753003@163.com>
2017-07-03 23:18:35 +0800

    正则处理模块修正完毕

--- regularAna.py
+++ regularAna.py
@@ -5,7 +5,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 def notNum(str):
-    matchNum = re.search("^[0-9]+$",str)
+    matchNum = re.search(ur"^[0-9]+$",str)
     if matchNum == None:
         return True
     return False
@@ -19,34 +19,34 @@ line = fr.readline()
 c = 0
 while line:
     # print c
-    headRe = re.search(r'^【(.*?)】', line, re.M | re.I)
+    line = line.decode("utf-8")
+    headRe = re.search(ur'^【(.*?)】', line, re.M | re.I)
     if headRe != None:
-        headRe = headRe.groups()[0].decode('utf-8')
+        headRe = headRe.groups()[0]
         lineRecord[c] = headRe
         if (headRe in diction) == False and notNum(headRe):
             diction.append(headRe)
             fw.write(headRe+'\n')
-            fwdir[headRe] = open("./model/group-key/" + headRe + ".txt", "a")
+            fwdir[headRe] = open("./model/group-key/" + headRe.decode("utf-8") + ".txt", "w")
             # print(headRe)
 
     else:  # 句首没有标签
-        tailRe = re.search(r'【([^】]*?)】$',line,re.M | re.I|re.U)
-        if tailRe == None:
-            tailRe = re.search(r'【([^【]*?)】$',line,re.M | re.I|re.U)
+        tailRe = re.search(ur'【([^【】]+)】$', line, re.U)
         if tailRe != None:
-            tailRe = tailRe.groups()[0].decode('utf-8')
+            tailRe = tailRe.groups()[0]
             lineRecord[c] = tailRe
             if (tailRe in diction) == False and notNum(tailRe):
                 diction.append(tailRe)
                 #print(tailRe)
-                fwdir[tailRe] = open("./model/group-key/" + tailRe + ".txt", "a")
+                #print line
+                fwdir[tailRe] = open("./model/group-key/" + tailRe.decode("utf-8") + ".txt", "w")
                 fw.write(tailRe+'\n')
         else:  # 句首句尾都没有标签
             lineRecord[c] = "KeyWordUndefined"
 
     line = fr.readline()
     c += 1
-fwdir["KeyWordUndefined"] = open("./model/group-key/" + "KeyWordUndefined" + ".txt", "a")
+fwdir["KeyWordUndefined"] = open("./model/group-key/" + "KeyWordUndefined" + ".txt", "w")
 fr.close()
 fw.close()
 
